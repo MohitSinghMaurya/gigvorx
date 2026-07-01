db24503d9c9d7b443e4c16fa31687edb7fd9947b
MohitSinghMaurya <mohitsinghm2025@gmail.com>
2026-07-01 12:41:45 +0530

    Fix React app entry file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "@/index.css";
+
 import App from "@/App";
+import "@/index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,10 +15,11 @@ const queryClient = new QueryClient({
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
