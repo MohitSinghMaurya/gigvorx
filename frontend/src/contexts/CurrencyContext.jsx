a020b07f196fe0efcd41a6f8e43c2f2f6a5891a3
MohitSinghMaurya <mohitsinghm2025@gmail.com>
2026-07-01 12:38:10 +0530

    Point duplicate currency context to shared provider

--- frontend/src/contexts/CurrencyContext.jsx
+++ frontend/src/contexts/CurrencyContext.jsx
@@ -1,26 +1,5 @@
-import { createContext, useContext, useEffect, useState } from "react";
-
-const CurrencyContext = createContext({ currency: "INR", setCurrency: () => {} });
-
-export function CurrencyProvider({ children }) {
-  const [currency, setCurrency] = useState("INR");
-
-  useEffect(() => {
-    fetch("https://ipapi.co/json/")
-      .then(res => res.json())
-      .then(data => {
-        if (data.country_code !== "IN") setCurrency("USD");
-      })
-      .catch(() => {});
-  }, []);
-
-  return (
-    <CurrencyContext.Provider value={{ currency, setCurrency }}>
-      {children}
-    </CurrencyContext.Provider>
-  );
-}
-
-export function useCurrency() {
-  return useContext(CurrencyContext);
-}
+export {
+  CurrencyProvider,
+  useCurrency,
+  default,
+} from "@/lib/CurrencyContext";
